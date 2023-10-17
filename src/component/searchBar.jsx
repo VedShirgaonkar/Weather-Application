7753f816f83751f80da653e9a3eb2cb07235d9b1
Ved Shirgaonkar <vedshirgaonkar@gmail.com>
2023-10-17 14:52:48 +0530

    fixed icons

--- src/component/searchBar.jsx
+++ src/component/searchBar.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import LocationIcon from '../assets/location-pin.png'
+import searchIcon from '../assets/search-interface-symbol.png'
 const searchBar=({setcityName})=>{
     const [ newcityname,setnewcityName] = useState('mumbai');
     const changeCityName=(e) =>{
@@ -13,15 +15,15 @@ const searchBar=({setcityName})=>{
      return(
      <div className="flex  items-center  m-5  text-black border-2 shadow-md  w-100% rounded-xl"  >
             <div className="m-4 h-auto w-10% mx-4">
-                <img src="../src/assets/location-pin.png" className=" h-8 w-full "  alt="image"/>  
+                <img src={LocationIcon} className=" h-8 w-full "  alt="image"/>  
             </div>
             <div className="inputBar h-100% flex-1 ">
              <form onSubmit={cityData}>
-             <input type="text" value={newcityname}  onChange={changeCityName}   className="ipText h-16 w-full ps-5 focus:outline-none text-lg" />
+             <input type="text" value={newcityname}  onChange={changeCityName}   className="ipText h-16 w-full ps-1 focus:outline-none text-lg" />
                 </form>   
             </div>
             <div className="m-4 h-auto mx-4  w-10% ">
-                <img src="../src/assets/search-interface-symbol.png" onClick={cityData} className=" h-8 w-full "  alt="image"/>  
+                <img src={searchIcon} onClick={cityData} className=" h-8 w-full "  alt="image"/>  
             </div>
         
     </div>
